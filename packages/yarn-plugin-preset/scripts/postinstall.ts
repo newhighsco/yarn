a596c2e2330588aa8ca3b59e180f3b31eceef526
Ben Edwards <hello@benedfit.com>
2026-02-20 11:29:22 +0000

    feat: automate install (#9)
    * feat: automate install
    
    Closes #1
    
    * chore: update files

---
+++ packages/yarn-plugin-preset/scripts/postinstall.ts
@@ -0,0 +1,13 @@
+import { execute } from '@yarnpkg/shell'
+
+import packageJson from '../package.json' with { type: 'json' }
+
+const { name, repository, version } = packageJson
+
+const tag = encodeURIComponent(`${name}@${version}`)
+const url = new URL(
+  `releases/download/${tag}/plugin-preset.js`,
+  repository.url.replace(/\.[^/.]+$/, '/')
+)
+
+execute(`yarn plugin import ${url.href}`)
